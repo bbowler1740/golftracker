9eaee96cf5dd357ca367ef10bb26e8eb355465d6
Vehikl <vehikl@dakal.local>
2023-06-21 09:03:14 -0400

    add rounds model and migration

---
+++ database/migrations/2023_06_21_082555_create_rounds_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    public function up(): void
+    {
+        Schema::create('rounds', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+//            $table->foreignId('course_id')->constrained()->cascadeOnDelete();
+//            $table->foreignId('weather')->constrained()->cascadeOnDelete();
+            $table->integer('score');
+            $table->integer('holes_played')->default(18);
+            $table->time('tee_off_time')->default(now());
+            $table->time('round_end_time');
+            $table->boolean('has_cart')->default(false);
+            $table->boolean('is_handicap_included')->default(true);
+            $table->enum('green_speed', ['N/A','slow', 'medium', 'fast'])->default('N/A');
+            $table->string('comments')->nullable();
+            $table->integer('beer_counter')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('rounds');
+    }
+};
