6ed85333251d319f387a2ce3776e20b2e49e4217
Vehikl <vehikl@dakal.local>
2023-06-28 08:57:47 -0400

    create CourseFactory

--- database/factories/CourseFactory.php
+++ database/factories/CourseFactory.php
@@ -4,20 +4,14 @@ namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
 
-/**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Course>
- */
 class CourseFactory extends Factory
 {
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
     public function definition(): array
     {
         return [
-            //
+            'location' => $this->faker->address,
+            'name' => $this->faker->company,
+            'cost_of_round' => $this->faker->numberBetween(50, 150),
         ];
     }
 }
