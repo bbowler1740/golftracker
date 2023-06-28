5bbf312d8c548b3e22c892478a77a3897cc8b739
Vehikl <vehikl@dakal.local>
2023-06-28 08:53:35 -0400

    create RoundFactory

---
+++ database/factories/RoundFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Round>
+ */
+class RoundFactory extends Factory
+{
+
+    public function definition(): array
+    {
+        return [
+            'user_id' => $this->faker->numberBetween(1, 10),
+            'score' => $this->faker->numberBetween(50, 75),
+            'holes_played' => $this->faker->randomElement([6, 9, 12, 18]),
+            'tee_off_time' => now(),
+            'round_end_time' => now()->addHours(3),
+            'has_cart' => $this->faker->boolean(),
+            'is_handicap_included' => $this->faker->boolean(),
+            'green_speed' => $this->faker->randomElement(['N/A', 'slow', 'medium', 'fast']),
+            'comments' => $this->faker->sentence(),
+            'beer_counter' => $this->faker->numberBetween(4, 24),
+        ];
+    }
+}
